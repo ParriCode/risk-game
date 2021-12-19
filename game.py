2fd0f931cae1d1b5167119a64117ab473954c33f
parri <90289658+ParriCode@users.noreply.github.com>
2021-12-19 11:10:31 +0100

    Update game.py

--- game.py
+++ game.py
@@ -1,11 +1,53 @@
 import colorama as cr
 import os
 tablero = {
-"eeuu"     : {"name" : "eeuu"     ,"armi": 0,"player": -1,"continent": "north america"},
-"china"    : {"name" : "china"    ,"armi": 0,"player": -1,"continent": "asia"},
-"alemania" : {"name" : "alemania" ,"armi": 0,"player": -1,"continent": "europa"},
-"brasil"   : {"name" : "brasil"   ,"armi": 0,"player": -1,"continent": "sur america"},
-"australia": {"name" : "australia","armi": 0,"player": -1,"continent": "oceania"}
+"alaska"                  : {"name" : "Alaska"                ,"armi": 0,"player": -1,"continent": "north_america"},
+"northwest_territory"     : {"name" : "Northwest Territory"   ,"armi": 0,"player": -1,"continent": "north_america"},
+"alberta"                 : {"name" : "Alberta"               ,"armi": 0,"player": -1,"continent": "north_america"},
+"ontario"                 : {"name" : "Ontario"               ,"armi": 0,"player": -1,"continent": "north_america"},
+"werstern united states"  : {"name" : "Werstern United States","armi": 0,"player": -1,"continent": "north_america"},
+"centeur america"         : {"name" : "Centeur America"       ,"armi": 0,"player": -1,"continent": "north_america"},
+"eastern united states"   : {"name" : "Eastern United States" ,"armi": 0,"player": -1,"continent": "north_america"},
+"quebec"                  : {"name" : "Quebec"                ,"armi": 0,"player": -1,"continent": "north_america"},
+"greenland"               : {"name" : "Greenland"             ,"armi": 0,"player": -1,"continent": "north_america"},
+
+"venezuela"               : {"name" : "Venezuela"   ,"armi": 0,"player": -1,"continent": "sourth_america"},
+"Peru"                    : {"name" : "Venezuela"   ,"armi": 0,"player": -1,"continent": "sourth_america"},
+"Argentina"               : {"name" : "Argentina"   ,"armi": 0,"player": -1,"continent": "sourth_america"},
+"brasil"                  : {"name" : "brasil"      ,"armi": 0,"player": -1,"continent": "sourth_america"},
+
+"iceland"                 : {"name" : "Iceland"        ,"armi": 0,"player": -1,"continent": "europe"},
+"Scandinavia"             : {"name" : "Scandinavia"    ,"armi": 0,"player": -1,"continent": "europe"},
+"gread_britain"           : {"name" : "Great Britain"  ,"armi": 0,"player": -1,"continent": "europe"},
+"werst_europe"            : {"name" : "Werst Europe"   ,"armi": 0,"player": -1,"continent": "europe"},
+"north_europe"            : {"name" : "North Europe"   ,"armi": 0,"player": -1,"continent": "europe"},
+"ukraini"                 : {"name" : "Ukraini"        ,"armi": 0,"player": -1,"continent": "europe"},
+"sourth_europe"           : {"name" : "Sourth Europe"  ,"armi": 0,"player": -1,"continent": "europe"},
+
+"north_africa"            : {"name" : "North Africa"   ,"armi": 0,"player": -1,"continent": "africa"},
+"egyipt"                  : {"name" : "North Africa"   ,"armi": 0,"player": -1,"continent": "africa"},
+"east_africa"             : {"name" : "North Africa"   ,"armi": 0,"player": -1,"continent": "africa"},
+"congo"                   : {"name" : "North Africa"   ,"armi": 0,"player": -1,"continent": "africa"},
+"sourth_africa"           : {"name" : "Sourth Africa"  ,"armi": 0,"player": -1,"continent": "africa"},
+"madalagascar"            : {"name" : "Madalasgar"     ,"armi": 0,"player": -1,"continent": "africa"},
+
+"ural"               : {"name" : "Ural"            ,"armi": 0,"player": -1,"continent": "asia"},
+"siberia"            : {"name" : "Siberia"         ,"armi": 0,"player": -1,"continent": "asia"},
+"afghanistan"        : {"name" : "Afghanistan"     ,"armi": 0,"player": -1,"continent": "asia"},
+"middle_east"        : {"name" : "Middle East"     ,"armi": 0,"player": -1,"continent": "asia"},
+"yakutsk"            : {"name" : "Yakutsk"         ,"armi": 0,"player": -1,"continent": "asia"},
+"irkust"             : {"name" : "Irkust"          ,"armi": 0,"player": -1,"continent": "asia"},
+"mongolia"           : {"name" : "Mongolia"        ,"armi": 0,"player": -1,"continent": "asia"},
+"china"              : {"name" : "China"           ,"armi": 0,"player": -1,"continent": "asia"},
+"india"              : {"name" : "India"           ,"armi": 0,"player": -1,"continent": "asia"},
+"sourteast_asia"     : {"name" : "SourthEast Asia" ,"armi": 0,"player": -1,"continent": "asia"},
+"japan"              : {"name" : "Japan"           ,"armi": 0,"player": -1,"continent": "asia"},
+"kaimchatka"         : {"name" : "Kaimchatka"      ,"armi": 0,"player": -1,"continent": "asia"},
+
+"indonesia"        : {"name" : "Australia"        ,"armi": 0,"player": -1,"continent": "oceania"},
+"new_guinea"       : {"name" : "Australia"        ,"armi": 0,"player": -1,"continent": "oceania"},
+"eastern_australia": {"name" : "Eastern Australia","armi": 0,"player": -1,"continent": "oceania"},
+"western_australia": {"name" : "Western Australia","armi": 0,"player": -1,"continent": "oceania"}
 }
 players = {}
 #clase del jugador
@@ -18,7 +60,9 @@ class player:
         self.country_list = []
     def AddCountry(self,country):
         self.country_list.append(country)
-    def NewArmi(self,n):
+    def AddNewArmy(self,n):
+        self.new_armi += n
+    def SubArmi(self,n):
         self.new_armi += n
         self.armi -= n
 
@@ -54,7 +98,7 @@ while True:
     print(cr.Fore.RED + disable_countries + cr.Fore.RESET)
     gate = False
     n_tropas = players[nid].new_armi
-    players[nid].NewArmi(-1)
+    players[nid].SubArmi(-1)
 
     if players[nid].new_armi > 0:
         print(f"Turno de {players[nid].name} // tropas restates {players[nid].new_armi}" )
@@ -73,5 +117,13 @@ while True:
    # print(tablero)
 nid = 0
 while True:
-    quit()
+    player_countries = players[nid].country_list
+    n_countries = len(player_countries)
+
+    players[nid].AddNewArmy(int(n_countries/5)+3)
+    print()
+    nid +=1
+    if nid > n_players: nid = 0
+
+
 
